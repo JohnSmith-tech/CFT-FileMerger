83112aded3d1f1202ec31766234a87e72c6cdcc5
johnsmith <iea2001@mail.ru>
2023-01-18 22:09:00 +0700

    Start app

--- src/main/java/org/cft/Main.java
+++ src/main/java/org/cft/Main.java
@@ -1,5 +1,37 @@
-package org.cft;public class Main {
+package org.cft;
+
+import org.cft.files.FileSplitter;
+import org.cft.files.merger.FileMerger;
+import org.cft.settings.SetupSettings;
+import org.cft.settings.enums.DataType;
+
+import java.io.IOException;
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SetupSettings settings = new SetupSettings(args);
+        FileSplitter fileSplitter = new FileSplitter(settings.getFiles(), settings.getDataType(), settings.getSortType());
+        try {
+
+            fileSplitter.splitAll();
+
+            FileMerger merger = new FileMerger(fileSplitter.getTempDir(), settings.getSortType());
+
+            if (settings.getDataType() == DataType.STRING) {
+                merger.mergeFilesByString(settings.getOutfile().getName(), fileSplitter.countFilesInNewDir());
+            } else {
+                merger.mergeFilesByInteger(settings.getOutfile().getName(), fileSplitter.countFilesInNewDir());
+            }
+
+            merger.mergeFilesByString(settings.getOutfile().getName(), fileSplitter.countFilesInNewDir());
+
+            fileSplitter.deleteDirectoryRecursion(fileSplitter.getTempDir());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
+
 }
